0f0607058aafd85a5a03c7d9ad68c8e84a73584b
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-23 14:08:55 +0300

    feat: add stubs

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -6,7 +6,7 @@ import {
   selectVisibleContacts,
 } from 'redux/contacts/selectors';
 import { Loader } from 'components/Loader';
-import { List } from '@mui/material';
+import { List, Typography } from '@mui/material';
 
 const ContactList = () => {
   const isLoading = useSelector(selectIsLoading);
@@ -24,10 +24,30 @@ const ContactList = () => {
       {isLoading && <Loader />}
 
       {!isLoading && filteredContacts.length === 0 && (
-        <p className="empty">There are no contacts in your phonebook.</p>
+        <Typography
+          variant="h3"
+          fontSize="20px"
+          marginLeft={2}
+          fontWeight={600}
+          color="#000"
+          textAlign="center"
+          margin="60px 0 20px"
+        >
+          There are no contacts in your phonebook :(
+        </Typography>
       )}
       {!isLoading && filteredContacts.length > 0 && (
-        <p className="contacts">You have {filteredContacts.length} contacts.</p>
+        <Typography
+          variant="h3"
+          fontSize="20px"
+          marginLeft={2}
+          fontWeight={700}
+          color="#000"
+          textAlign="center"
+          margin="20px 0 20px"
+        >
+          You have {filteredContacts.length} contacts.
+        </Typography>
       )}
       {!isLoading &&
         filteredContacts?.map(({ name, id, number }) => (

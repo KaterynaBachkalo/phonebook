cd5759670ae0a4ae367dd2fab42733a304474d5d
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-23 14:09:16 +0300

    fix: whitelist

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,7 +17,7 @@ import storage from 'redux-persist/lib/storage';
 const authConfig = {
   key: 'auth',
   storage,
-  whitelist: ['token'],
+  whitelist: ['accessToken'],
 };
 
 export const store = configureStore({

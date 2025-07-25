7d51c5f42df62b141805c0ef9de86f7e10d5a8f4
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-25 15:23:24 +0300

    fix

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -10,8 +10,9 @@ export const selectFilter = state => state.filter;
 
 export const selectVisibleContacts = createSelector(
   [selectContacts, selectFilter],
-  (contacts, filter) => {
+  (contacts = [], filter) => {
     const normalizedFilter = filter.toLowerCase().trim();
+
     return contacts?.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
